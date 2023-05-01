b02c342f70ab43622941e2dfd409bfe974c88685
afterAcer <after197918@gmail.com>
2023-05-02 00:06:01 +0700

    Create Edit&Create Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace BulkyWeb.Controllers
 {
@@ -18,5 +19,61 @@ namespace BulkyWeb.Controllers
             List<Category> objcategoryList = _db.Categories.ToList();
             return View(objcategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order Cannot exactly Match the name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "badname")
+            {
+                ModelState.AddModelError("", "you cannot use this name its too bad!!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.CategoryId == 0)
+            {
+                Console.WriteLine("id = 0");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -8,8 +9,12 @@ namespace BulkyWeb.Models
         public int CategoryId { get; set; }
 
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
 
+        [DisplayName("Display order")]
+        [Range(1, 100,ErrorMessage ="Must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
